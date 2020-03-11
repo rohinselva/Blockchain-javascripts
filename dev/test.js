028be9c3b6ef691d705a7822d74ce3ba51c855aa
Rohin Selva <rohin.selva@synapse.com>
2020-03-11 15:41:50 +0530

    BlockChain-Javascript

---
+++ dev/test.js
@@ -0,0 +1,50 @@
+const Blockchain=require('./blockchain');
+
+const bitcoin =new Blockchain();
+
+console.log(bitcoin);
+
+// const previousBlockHash='UFGVRUGUYGGUVR';
+// const currentBlockData=
+// [
+
+// {
+// 	amount:10,
+// 	sender:'UGCIYUHUICUY',
+// 	recipient:'VU7TY56FYU'
+// },
+// {
+// 	amount:20,
+// 	sender:'NIUCEUGFIU378',
+// 	recipient:'NWUIFTBUGIWV'
+// },
+// {
+// 	amount:30,
+// 	sender:'OCIEHY84UH4',
+// 	recipient:'DUICYGEHIU'
+// },
+// ];
+
+// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,12253));
+//console.log(bitcoin.proofOfWork(previousBlockHash,currentBlockData));
+//const nonce= 100;
+
+//console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce));
+
+// bitcoin.createNewBlock(1207,'12kjjhkxw','hijhiue');
+
+// bitcoin.createNewTransaction(200,'Rohin4001','Suma4002');
+
+// bitcoin.createNewBlock(023,'12hkjwegkh3','bkuhkei');
+
+// bitcoin.createNewTransaction(100,'Sam001','Sammy4002');
+// bitcoin.createNewTransaction(400,'A4001','B4002');
+// bitcoin.createNewTransaction(500,'C4001','D4002');
+
+// bitcoin.createNewBlock(065,'ruifhkjwegkh3','huicguh');
+
+//console.log(bitcoin.chain[2]); //for printing the ttransactions within the CREATEBLOCK
+//console.log(bitcoin); // this shows the [ARRAY] in the transactions field of the CREATE BLOCK
+
+
+
